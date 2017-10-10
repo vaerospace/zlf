b5a6b13e953ea43725dde7d7370c1e2c836feee0
vaerospace <vaeroni@gmail.com>
2017-10-11 00:46:57 +0200

    Master Ascii Character Handler

--- splitfile2.f
+++ splitfile2.f
@@ -2,21 +2,12 @@ needs baaall2017.f
 create woxup
 \ variable zlf   variable zlf1  variable zlf2   variable intzed
 variable a    variable b   variable c variable d variable e
-\ : ax   a @ . b @ . c @ . ;
+
 \ : ts1 ( intitial zlf char pile ) cr woxup 512  expect  cr woxup span @  type  cr span ?  # #  ." letters typed"  ;
 
 \ put char a in zlf as 9 first the 7 requires splitting and knowing how many digits in the ascii char code
 
-\ : findzlf  expect woxup span @   100 /mod
-\                            if 10 < zlf !
-\                                else
-\                                   intzed !
-\                                       then 10 /mod  zlf1 ! zlf @ . zlf1 @ . intzed @ .
-\                                        ;
-\ : tst  cr woxup 515 expect cr woxup span @ type . ;
-\ : tsf   ( intitial zlf char pile )  expect  10 /mod .  . ;
-\ : tsd   expect    zlf @ . ;
-\ : tsa   cr woxup 512 expect  cr woxup  span @ type cr span ? ;
+
 
 : >string  >r dup >r abs s>d <# #s r> sign #> r@ char+ swap dup >r cmove r> r> c! ;
 ( n a -- )
@@ -30,18 +21,15 @@ variable a    variable b   variable c variable d variable e
 \ -1024 num$ >string
 
 \ num$ count type cr
-\ : ?repeats  \ anything on stack if so is this char = last char ?
+\ : ?repeats  \ anything on stack if so is this char = last char ? not finished yet
 : shw      woxup  >string woxup count  c ! ( the number of digits ) drop ;  \  the code that worked
 
-\ : shx      woxup 3 +  c@  d !  ;
+
 
 : woks  a @ . b @ . c @ . ;
 : stt      dup e !  ;
 : zed1   stt shw ;
 
-  \ count c !  not C@ store not worling ascii123  fix IT now
-\ flush woxip buffer to check how many digits in ascii char code 1 2 or 3
-\  : doasciicount   case
 : doupzip case
  48 of axx  endof
  49 of bxx endof
@@ -80,23 +68,12 @@ variable a    variable b   variable c variable d variable e
                                      ;
 : ascount woxup count ascii123 ;  \ how many ascii code chars to bang onto S1
 
-\ : whichascii    c@   3 = if doupzipasciichar3
-     \                      2 =  if doupzipasciichar2
-        \                       1 =  if doupzipasciichar1
-             \                 then then then ;
+
 
 : sh1      doupzipasciichar1 ;
 : shr      doupzipasciichar2  ;
 : sht      doupzipasciichar3  ;
 : test shw shr sht doaero ;
 : runx zed1 ascount ; \ main start word need number between 0 and 255 use : zlfclear   to zero stack
+\  to test  type:      244 runx      or some ascii decimal code     TO check result type:     doaero
 
-\     woxup 1 +  c@ emit 9 ok
-
-
-\  the   solution  97 /mod  ok.
-\ . 0  ok
-\ 97 10 /mod  ok..
-\ . . 9 7  ok
-\ 111 10 /mod  ok..
-\  . . 11 1  ok
